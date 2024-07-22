775c22fd03d525d2b7e5ef52727adf1ad4f01734
Xincheng Yu <66622804+XCYu-0903@users.noreply.github.com>
2024-07-22 18:31:58 +0800

    Update inversionPair.cpp

--- Divide and Conquer/inversionPair.cpp
+++ Divide and Conquer/inversionPair.cpp
@@ -1,3 +1,25 @@
+/*
+Problem Description
+
+In stock market analysis, contrarian trading is a common investment strategy that aims to invest in the opposite direction of the prevailing market trend. Based on this investment strategy, financial analysts need to pay attention to moments when stock prices suddenly drop. To effectively track these situations, we define the concept of stock market inversion pairs, where an inversion pair is formed if the stock price on one day is higher than the stock price on a subsequent day.
+
+Given an integer array *prices*, where each element represents the closing price of a stock on a particular day, design a program that uses a divide-and-conquer algorithm to calculate the total number of inversion pairs in the array.
+
+
+Example 1
+
+Input: 
+5  
+9 7 5 4 6
+
+Output: 
+8
+
+Explanation:  
+The inversion pairs in this example are: (9, 7), (9, 5), (9, 4), (9, 6), (7, 5), (7, 4), (7, 6), (5, 4)
+*/
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
